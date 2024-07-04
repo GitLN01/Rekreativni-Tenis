22a9a9187b278bd15afeff9493aec8b14f354428
Ana Vukajlović <avukajlovic542@gmail.com>
2024-07-04 15:57:17 +0200

    Add classes for design

---
+++ view-klubovi/partials/navigacija.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Klubovi</title>
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap5.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./css/error-validacija.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap5.min.css">
+  <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+</head>
+
+<body>
+
+  <main>
+    <h1 class="visually-hidden">Sportski klubovi</h1>
+
+    <div class="d-flex flex-column flex-shrink-0 p-3 text-white " style="width: 280px; background-color:#6386a3">
+      <a href="pocetna.php" class=" text-center d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+
+        <span class="fs-4 text-center">TENISKI KLUBOVI</span>
+      </a>
+      <hr>
+      <ul class="nav nav-pills flex-column mb-auto">
+        <li>
+          <a href="./tereni.php" class="nav-link text-white">
+
+            Tereni
+          </a>
+        </li>
+        <li>
+          <a href="./rezervacije.php" class="nav-link text-white">
+
+            Rezervacija
+          </a>
+        </li>
+        <li>
+          <a href="./rezultati.php" class="nav-link text-white">
+
+            Rezultati
+          </a>
+        </li>
+        <li>
+          <a href="./igraci.php" class="nav-link text-white">
+
+            Igrači
+          </a>
+        </li>
+
+      </ul>
+      <hr>
+
+    </div>
+
+    <div class="b-example-divider"></div>
