140c27ff1106ebd75db41c89ca19dd97d527f186
Lazar Nikitovic <lazonikitovic@gmail.com>
2024-07-06 19:03:31 +0200

    Update freegrounds.php

--- view-gosti/freegrounds.php
+++ view-gosti/freegrounds.php
@@ -5,45 +5,49 @@ require_once '../DAO/TerenDAO.php';
 require_once '../model/Teren.php';
 
 $terenDAO = new TerenDAO();
-$tereni = $terenDAO->getAllTereni()
+$tereni = $terenDAO->getAllTereni();
 ?>
 
-<div class="container" style="display: flex; flex-direction: row; background-repeat: no-repeat; background-size: cover;">
-    <div class="row">
-        <?php
+<div class="container-fluid" style="background-repeat: no-repeat; background-size: cover; background-image: url('images/carousel3.jpg'); overflow-y: scroll; height: 100vh;">
+    <h2 class="m-4 text-white">Prikaz svih terena</h2>
 
-        $msg = isset($_SESSION['msg']) ? $_SESSION['msg'] : '';
-        if (!empty($msg)) {
+    <?php
+    $msg = isset($_SESSION['msg']) ? $_SESSION['msg'] : '';
+    if (!empty($msg)) {
         ?>
-            <div class="toast show  position-fixed bottom-0 end-0 p-3" style="z-index: 11">
-                <div class="toast-header ">
-                    <strong class="me-auto"></strong>
-                    <button type="button" class="btn-close" data-bs-dismiss="toast"></button>
-                </div>
-                <div class="toast-body">
-                    <p><?= $msg ?></p>
-                </div>
+        <div class="toast show position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+            <div class="toast-header">
+                <strong class="me-auto"></strong>
+                <button type="button" class="btn-close" data-bs-dismiss="toast"></button>
             </div>
-        <?php }
-        ?>
-        <?php foreach ($tereni as $item) { if($item['zauzet']==0) {?>
-            <div class="card bg-dark" style="width: 18rem; height: 25rem; margin: 0.5rem; color: white">
-            <?php $image = "./images/" . $item['vrsta_podloge'] . ".jpg";?>
-                <img style="height:100px; padding-top: 5px;" src="<?php echo $image; ?>"  class="card-img-top" alt="teren">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $item['naziv'] ?></h5>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Lokacija: <?= $item['lokacija'] ?></li>
-                    <li class="list-group-item">Vrsta podloge: <?= $item['vrsta_podloge'] ?></li>
-                    <li class="list-group-item">Kapacitet: <?= $item['kapacitet'] ?></li>
-                    <li class="list-group-item">Id kluba: <?= $item['id_kluba'] ?></li>
-                </ul>
+            <div class="toast-body">
+                <p><?= $msg ?></p>
             </div>
-        <?php }} ?>
+        </div>
+    <?php } ?>
+
+    <div class="row justify-content-center">
+        <?php foreach ($tereni as $item) { ?>
+            <?php if ($item['zauzet'] == 0) { ?>
+                <div class="col">
+                    <div class="card bg-success text-white m-2" style="width: 18rem;">
+                        <?php $image = "./images/" . $item['vrsta_podloge'] . ".jpg"; ?>
+                        <img src="<?= $image ?>" class="card-img-top" style="object-fit: cover; height: 200px;" alt="teren">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $item['naziv'] ?></h5>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item bg-success text-white">Lokacija: <?= $item['lokacija'] ?></li>
+                            <li class="list-group-item bg-success text-white">Vrsta podloge: <?= $item['vrsta_podloge'] ?></li>
+                            <li class="list-group-item bg-success text-white">Kapacitet: <?= $item['kapacitet'] ?></li>
+                            <li class="list-group-item bg-success text-white">Id kluba: <?= $item['id_kluba'] ?></li>
+                        </ul>
+                    </div>
+                </div>
+            <?php } ?>
+        <?php } ?>
     </div>
-   <!-- <a href="grounds.php"><img src="./images/back.png" class="m-auto d-block" style="height: 30px;" alt=""></a> -->
+
 </div>
-<?php
-require_once './partials/footer.php';
-?>
+
+<?php require_once './partials/footer.php'; ?>
