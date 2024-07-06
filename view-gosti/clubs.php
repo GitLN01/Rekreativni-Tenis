5173638137a98d859128a8fac582b30e91132cdf
Lazar Nikitovic <lazonikitovic@gmail.com>
2024-07-06 17:52:48 +0200

    Update class clubs.php

--- view-gosti/clubs.php
+++ view-gosti/clubs.php
@@ -8,13 +8,14 @@ $klubDAO = new SportskiKlubDAO();
 $klubovi = $klubDAO->getAllSportskiKlubovi();
 ?>
 
-<div class="container" style="overflow:scroll;">
+<div class="container-fluid"
+    style="overflow: hidden; background-repeat: no-repeat; background-size: cover; background-image: url('images/carousel1.jpg');">
     <div class="row">
         <?php
 
         $msg = isset($_SESSION['msg']) ? $_SESSION['msg'] : '';
         if (!empty($msg)) {
-        ?>
+            ?>
             <div class="toast show  position-fixed bottom-0 end-0 p-3" style="z-index: 11">
                 <div class="toast-header ">
                     <strong class="me-auto"></strong>
@@ -28,42 +29,41 @@ $klubovi = $klubDAO->getAllSportskiKlubovi();
         ?>
         <div class="container m-3" style="width: 90%;">
 
-            <table class="table table-bordered  text-center  table-striped  table-sm mt-3" cellspacing="0" style=" margin-left:auto;margin-right:auto" id="dtBasicExample">
+            <table class="table table-bordered  text-center  table-striped  table-sm mt-3" cellspacing="0"
+                style=" margin-left:auto;margin-right:auto" id="dtBasicExample">
                 <thead class="table-dark sticky-top bg-white">
                     <tr>
-                        <th class="th-sm">Id</th>
-                        <th class="th-sm">Naziv</th>
-                        <th class="th-sm">Adresa</th>
+                        <th class="th-sm" style="background-color: #343a40; color: white;">Id</th>
+                        <th class="th-sm" style="background-color: #343a40; color: white;">Naziv</th>
+                        <th class="th-sm" style="background-color: #343a40; color: white;">Adresa</th>
                     </tr>
                 </thead>
                 <tbody>
-
-
-                    <?php foreach ($klubovi as $item) : ?>
+                    <?php foreach ($klubovi as $item): ?>
                         <tr>
-                            <td><?= $item['id'] ?></td>
-                            <td><?= $item['naziv'] ?></td>
-                            <td><?= $item['adresa'] ?></td>
+                            <td style="background-color: #21ad09;color:white;font-weight:bold;"><?= $item['id'] ?></td>
+                            <td style="background-color: #21ad09;color:white;font-weight:bold;"><?= $item['naziv'] ?></td>
+                            <td style="background-color: #21ad09;color:white;font-weight:bold;"><?= $item['adresa'] ?></td>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
 
-            
-        </div>
-        <script>
-            $(document).ready(function() {
-                $('#dtBasicExample').DataTable();
-                $('.dataTables_length').addClass('bs-select');
-                $('.pagination').hide();
-                $('#dtBasicExample_info').hide();
-                $('#dtBasicExample_paginate').hide();
-                $('#dtBasicExample_length').hide();
 
-            });
-        </script>
+    </div>
+    <script>
+        $(document).ready(function () {
+            $('#dtBasicExample').DataTable();
+            //$('.dataTables_length').addClass('bs-select');
+            //$('.pagination').hide();
+            //$('#dtBasicExample_info').hide();
+            //$('#dtBasicExample_paginate').hide();
+            $('#dtBasicExample_length').hide();
 
-        <?php
-        require_once './partials/footer.php';
-        ?>
+        });
+    </script>
+
+    <?php
+    require_once './partials/footer.php';
+    ?>
