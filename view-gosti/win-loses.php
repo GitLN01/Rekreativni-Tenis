ca6c48aad1c0d2c4b204a7a5e05c9fa8ff933609
Lazar Nikitovic <lazonikitovic@gmail.com>
2024-07-06 20:58:42 +0200

    Update both classes for show result function

--- view-gosti/win-loses.php
+++ view-gosti/win-loses.php
@@ -8,65 +8,55 @@ $korisnikDAO = new KorisnikDAO();
 $igraci = $korisnikDAO->getAllIgraciSortWL();
 ?>
 
-<div class="container">
-    <div class="row">
-        <?php
+<div class="container-fluid" style="background-image: url('./images/carousel3.jpg'); background-repeat: no-repeat; background-size: cover; overflow: auto; height: 100vh;">
+    <h2 class="m-4 text-white">Rangiranje prema broju pobeda i poraza</h2>
 
-        $msg = isset($_SESSION['msg']) ? $_SESSION['msg'] : '';
-        if (!empty($msg)) {
-        ?>
-            <div class="toast show  position-fixed bottom-0 end-0 p-3" style="z-index: 11">
-                <div class="toast-header ">
-                    <strong class="me-auto"></strong>
-                    <button type="button" class="btn-close" data-bs-dismiss="toast"></button>
-                </div>
-                <div class="toast-body">
-                    <p><?= $msg ?></p>
-                </div>
+    <?php
+    $msg = isset($_SESSION['msg']) ? $_SESSION['msg'] : '';
+    if (!empty($msg)) {
+    ?>
+        <div class="toast show position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+            <div class="toast-header">
+                <strong class="me-auto"></strong>
+                <button type="button" class="btn-close" data-bs-dismiss="toast"></button>
             </div>
-        <?php }
-        ?>
-        <div class="container m-3" style="width: 90%;">
-        <h3>Rangiranje prema broju pobeda i poreza</h3>
-        <a href="win.php">Svi rezultati</a><br>
-        <a href="win.php">Rangiranje prema broju pobeda</a><br>
-             
-            <table class="table table-bordered  text-center  table-striped  table-sm mt-3" cellspacing="0" style=" margin-left:auto;margin-right:auto" id="dtBasicExample">
-                <thead class="table-dark sticky-top bg-white">
+            <div class="toast-body">
+                <p><?= $msg ?></p>
+            </div>
+        </div>
+    <?php } ?>
+
+    <div class="container mt-3 mb-3 p-3" style="background-color: rgba(255, 255, 255, 0.8); border-radius: 10px;">
+        <table class="table table-bordered table-striped table-hover text-center mt-3">
+            <thead class="table-dark sticky-top bg-white">
+                <tr>
+                    <th class="th-sm">Ime</th>
+                    <th class="th-sm">Prezime</th>
+                    <th class="th-sm">Visina</th>
+                    <th class="th-sm">Godine</th>
+                    <th class="th-sm">Pobede</th>
+                    <th class="th-sm">Porazi</th>
+                    <th class="th-sm">Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($igraci as $item) : ?>
                     <tr>
-                        <th class="th-sm">Ime</th>
-                        <th class="th-sm">Prezime</th>
-                        <th class="th-sm">Visina</th>
-                        <th class="th-sm">Godine</th>
-                        <th class="th-sm">Pobede</th>
-                        <th class="th-sm">Porazi</th>
-                        <th class="th-sm">Email</th>
-
+                        <td><?= $item['ime'] ?></td>
+                        <td><?= $item['prezime'] ?></td>
+                        <td><?= $item['visina'] ?></td>
+                        <td><?= $item['godine'] ?></td>
+                        <td><?= $item['pobeda'] ?></td>
+                        <td><?= $item['porazi'] ?></td>
+                        <td><?= $item['email'] ?></td>
                     </tr>
-                </thead>
-                <tbody>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 
-
-                    <?php foreach ($igraci as $item) : ?>
-                        <tr>
-                            <td><?= $item['ime'] ?></td>
-                            <td><?= $item['prezime'] ?></td>
-                            <td><?= $item['visina'] ?></td>
-                            <td><?= $item['godine'] ?></td>
-                            <td><?= $item['pobeda'] ?></td>
-                            <td><?= $item['porazi'] ?></td>
-                            <td><?= $item['email'] ?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
-
+    <h3><a href="results.php" class="m-4 text-white" style="text-decoration:none;">Svi rezultati</a></h3>
+    <h3><a href="win.php" class="m-4 text-white" style="text-decoration:none;">Rangiranje prema broju pobeda</a></h3>
 </div>
-            
-        </div>
-   
 
-        <?php
-        require_once './partials/footer.php';
-        ?>
+<?php require_once './partials/footer.php'; ?>
