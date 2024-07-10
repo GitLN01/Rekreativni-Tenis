ca8f0c31a6ae497ba43cecd3fb24556478920510
Lazar Nikitovic <lazonikitovic@gmail.com>
2024-07-10 20:42:44 +0200

    Update profil.php

--- view-klubovi/profil.php
+++ view-klubovi/profil.php
@@ -3,49 +3,44 @@ require_once './partials/navigacija.php';
 require_once '../model/Korisnik.php';
 session_start();
 
-// Check session timeout
 $vremeActivneSesije = isset($_SESSION['last_active1']) ? $_SESSION['last_active1'] : 0;
 if (time() - $vremeActivneSesije < 10 * 60) {
-    // Check if user is logged in
     if (!isset($_SESSION['loginK'])) {
         header("Location: ./login.php");
         exit();
     } else {
-        // Deserialize user object from session
-        $korisnik = isset($_SESSION['loginK']) ? unserialize($_SESSION['loginK']) : null;
+        $korisnik = isset($_SESSION['loginK']) ? unserialize($_SESSION['loginK']) : new Korisnik();
 
-        // Verify if $korisnik is an instance of Korisnik
-        if (!($korisnik instanceof Korisnik)) {
-            // Handle the error appropriately
-            echo "Error: Invalid user data.";
-            exit();
-        }
-?>
+        ?>
         <div class="container" style="background-color: #eee;">
             <section>
-                <form action="../controller-klubovi/ProfilController.php?action=edit" method="post" id="formProfil" enctype='multipart/form-data'>
+                <form action="../controller-klubovi/ProfilController.php?action=edit" method="post" id="formProfil"
+                    enctype='multipart/form-data'>
                     <div class="container py-5">
                         <div class="row">
                             <div class="col-lg-4">
                                 <div class="card mb-4">
                                     <div class="card-body text-center">
                                         <?php
-                                        // Check if user has a profile picture
-                                        if (empty($korisnik->getProfilnaSlika())) {
-                                        ?>
-                                            <img src="./images/user128.png" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
-                                        <?php
+                                        if (!isset($korisnik['profilna_slika']) || empty($korisnik['profilna_slika'])) {
+                                            ?>
+                                            <img src="./images/user128.png" alt="avatar" class="rounded-circle img-fluid"
+                                                style="width: 150px;">
+                                            <?php
                                         } else {
-                                            echo '<img style="width: 150px;" class="rounded-circle" src="data:image/png;base64,' . base64_encode($korisnik->getProfilnaSlika()) . '" />';
+                                            echo '<img style="width: 150px;" class="rounded-circle" src="data:image/png/image/jpeg;base64,' . base64_encode($korisnik['profilna_slika']) . '" />';
                                         }
                                         ?>
                                         <h5 class="my-3">
-                                            <?php echo $korisnik->getIme() . ' ' . $korisnik->getPrezime() ?>
+                                            <?php echo $korisnik['ime'] . ' ' . $korisnik['prezime'] ?>
                                         </h5>
-                                        <p class="text-muted mb-1">Radnik u teniskom klubu</p>
+                                        <p class="text-muted mb-1">Korisnik u teniskom klubu</p>
                                         <input type="file" class="btn form-control btn-sm" name="slika" />
                                         <p class="text-muted mb-4"></p>
                                     </div>
+                                </div>
+                                <div class=" mb-4 mb-lg-0">
+
                                 </div>
                             </div>
                             <div class="col-lg-8">
@@ -56,7 +51,8 @@ if (time() - $vremeActivneSesije < 10 * 60) {
                                                 <p class="mb-0">Ime</p>
                                             </div>
                                             <div class="col-sm-9">
-                                                <input type="text" class="form-control" name="ime" value="<?php echo $korisnik->getIme() ?>">
+                                                <input type="text" class="form-control" name="ime" id=""
+                                                    value="<?php echo $korisnik['ime'] ?>">
                                             </div>
                                         </div>
                                         <hr>
@@ -65,7 +61,8 @@ if (time() - $vremeActivneSesije < 10 * 60) {
                                                 <p class="mb-0">Prezime</p>
                                             </div>
                                             <div class="col-sm-9">
-                                                <input type="text" class="form-control" name="prezime" value="<?php echo $korisnik->getPrezime() ?>">
+                                                <input type="text" class="form-control" name="prezime"
+                                                    value="<?php echo $korisnik['prezime'] ?>">
                                             </div>
                                         </div>
                                         <hr>
@@ -74,12 +71,17 @@ if (time() - $vremeActivneSesije < 10 * 60) {
                                                 <p class="mb-0">Email</p>
                                             </div>
                                             <div class="col-sm-9">
-                                                <input type="email" class="form-control" name="email" value="<?php echo $korisnik->getEmail() ?>">
-                                                <input type="hidden" name="id" value="<?= $korisnik->getId() ?>">
+                                                <input type="email" class="form-control" name="email"
+                                                    value="<?php echo $korisnik['email'] ?>">
+                                                    <input type="hidden" name="id"
+                                                    value="<?= isset($korisnik['id']) ? $korisnik['id'] : '' ?>">
                                             </div>
                                         </div>
                                         <hr>
-                                        <input type="submit" value="Sačuvaj promene" class="btn btn-primary btn-sm">
+                                        <br>
+                                        <input type="submit" value="Sačuvaj promene" class="btn btn-primary btn-md">
+                                        <div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -92,17 +94,15 @@ if (time() - $vremeActivneSesije < 10 * 60) {
         <script src="./js/jquery.validate.min.js"></script>
         <script src="./js/validacija-profila.js"></script>
 
-<?php
+        <?php
     }
 } else {
-    // Destroy session if inactive
     session_unset();
     session_destroy();
     header("Location: login.php");
     exit();
 }
 
-// Update session activity time
 $_SESSION['last_active1'] = time();
 require_once './partials/footer.php';
 ?>
